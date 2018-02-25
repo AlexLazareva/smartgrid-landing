ecf03a3dd1508c289261277142e5b9d55eddaf22
Alex Lazareva <alex.lazareva25@yandex.ru>
2018-02-25 16:31:24 +0300

    init project

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+
+gulp.task('default', function () {
+    console.log(10);
+});
