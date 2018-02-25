1d9890a015c077b69eab1fac5bc6e7ef90865817
Alex Lazareva <alex.lazareva25@yandex.ru>
2018-02-25 16:49:43 +0300

    remove sass from project

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,22 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
-const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const less = require('gulp-less');
+const browserSync = require('browser-sync');
+const gcmq = require('gulp-group-css-media-queries');
 
-gulp.task('default', function () {
-    console.log(10);
+const config = {
+    root: './src/',
+    html: {
+        src: 'index.html'
+    },
+    css: {
+        watch: 'less/**/*.less',
+        src: 'less/+(styles).less',
+        dest: 'css'
+    }
+};
+
+gulp.task('build', function () {
+    gulp.src()
 });
